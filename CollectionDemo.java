d735e500e57f179bb4a6976063d9ef540eb761f1
Parth Joshi <parthjoshi425@gmail.com>
2025-09-25 17:26:23 +0530

    Test JAVA Second Project

---
+++ CollectionDemo.java
@@ -0,0 +1,154 @@
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+class CollectionDemo{
+	public static void main(String[] args){
+			LinkedList<String> linkedList = new LinkedList<>();	
+		linkedList.add("name1");
+		linkedList.addFirst("start");
+        linkedList.addLast("end");
+        linkedList.remove("name1");
+
+        System.out.println(linkedList.indexOf("start"));   
+
+
+        Set<String> set = new HashSet<>();
+        set.add("name1");
+        set.add("name2");
+        set.add("name1");
+
+        for(String s : set) 
+            System.out.println(s);
+
+        System.out.println("-----------------------------------------------------------------------------------");
+
+        Set<String> s = new LinkedHashSet<>();
+        s.add("name1");
+        s.add("name2");
+        s.add("name3");
+        s.add("name2");
+
+        for(String s1 : s)
+            System.out.println(s1);
+
+
+        System.out.println("-----------------------------------------------------------------------------------");
+
+
+        Set<String> tree = new TreeSet<>();
+        tree.add("name2");
+        tree.add("name1");
+        tree.add("name3");
+
+        for(String s2: tree)
+            System.out.println(s2);
+
+
+        Map<Integer, String> map = new HashMap<>();
+        // Adding key-value pairs
+        map.put(1, "Java");
+        map.put(2, "Python");
+        map.put(3, "C++");
+        map.put(2, "Go"); // Key 2 updated with new value "Go"
+        map.put(null, "NullKey"); // Null key
+        map.put(4, null);         // Null value
+        map.put(5, null);         // Another null value
+
+        // Printing the entire map
+        System.out.println("Map: " + map);
+
+        // Accessing a value
+        System.out.println("Value for key 1: " + map.get(1));
+
+        // Iterating using for-each (entrySet)
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+
+        // Iterating keys only
+        for (Integer key : map.keySet()) {
+            System.out.println("Key: " + key);
+        }
+
+        // Iterating values only
+        for (String value : map.values()) {
+            System.out.println("Value: " + value);
+        }
+
+        // Checking if a key exists
+        System.out.println("Contains key 3? " + map.containsKey(3));
+
+        // Checking if a value exists
+        System.out.println("Contains value 'Java'? " + map.containsValue("Java"));
+
+        // Removing a key
+        map.remove(4);
+        System.out.println("After removing key 4: " + map);
+
+
+
+        // Create LinkedHashMap
+        Map<Integer, String> lhm = new LinkedHashMap<>();
+
+        // Add key-value pairs
+        lhm.put(3, "C++");
+        lhm.put(1, "Java");
+        lhm.put(2, "Python");
+        lhm.put(4, "Go");
+
+        // Print map
+        System.out.println("LinkedHashMap: " + lhm);
+
+        // Iterating using for-each
+        for (Map.Entry<Integer, String> entry : lhm.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+
+
+
+         // TreeMap with natural ordering
+        TreeMap<Integer, String> tmap = new TreeMap<>();
+
+        tmap.put(3, "C++");
+        tmap.put(1, "Java");
+        tmap.put(4, "Go");
+        tmap.put(2, "Python");
+
+        // Print map
+        System.out.println("TreeMap: " + tmap);
+
+        // Iterating through entries
+        for (Map.Entry<Integer, String> entry : tmap.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+
+        // Some TreeMap methods
+        System.out.println("First Key: " + tmap.firstKey());
+        System.out.println("Last Key: " + tmap.lastKey());
+        System.out.println("Higher Key(2): " + tmap.higherKey(2));
+        System.out.println("Lower Key(3): " + tmap.lowerKey(3));
+
+        Queue<Integer> queue = new LinkedList(); 
+        queue.add(21);
+        queue.add(14);
+        System.out.println(queue.poll());
+
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(12);
+        pq.add(54);
+        pq.add(47);
+        System.out.println(pq.poll()); //12
+
+	}	
+}
---
+++ OptionalExamples.java
@@ -0,0 +1,97 @@
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+public class OptionalExamples {
+
+    public static void main(String[] args) {
+
+        // 1. empty()
+        Optional<String> emptyOpt = Optional.empty();
+        System.out.println("emptyOpt: " + emptyOpt);
+
+// 2. of()
+        Optional<String> opt = Optional.of("Hello");
+        System.out.println("opt: " + opt);
+
+// 3. ofNullable()
+        Optional<String> nullableOpt = Optional.ofNullable(null);
+        System.out.println("nullableOpt: " + nullableOpt);
+
+// 4. isPresent()
+        System.out.println("opt is present: " + opt.isPresent());
+        System.out.println("emptyOpt is present: " + emptyOpt.isPresent());
+
+// 5. ifPresent()
+        opt.ifPresent(value -> System.out.println("Value in opt: " + value));
+
+// 6. isEmpty() (Java 11+)
+        System.out.println("emptyOpt is empty: " + emptyOpt.isEmpty());
+
+// 7. get()
+        try {
+            System.out.println("Value from opt: " + opt.get());
+            System.out.println("Value from emptyOpt: " + emptyOpt.get()); // Will throw NoSuchElementException
+        } catch (Exception e) {
+            System.out.println("Exception from emptyOpt.get(): " + e);
+        }
+
+// 8. orElse()
+        String value = opt.orElse("Default Value");
+        System.out.println("orElse value: " + value);
+        String defaultValue = emptyOpt.orElse("Default Value");
+        System.out.println("orElse default value: " + defaultValue);
+
+// 9. orElseGet()
+        String valueWithSupplier = opt.orElseGet(() -> "Default Value from Supplier");
+        System.out.println("orElseGet value: " + valueWithSupplier);
+        String defaultValueWithSupplier = emptyOpt.orElseGet(() -> "Default Value from Supplier");
+        System.out.println("orElseGet default value: " + defaultValueWithSupplier);
+
+// 10. orElseThrow()
+        try {
+            String throwValue = emptyOpt.orElseThrow(() -> new IllegalArgumentException("Value not present"));
+        } catch (Exception e) {
+            System.out.println("Exception from orElseThrow: " + e);
+        }
+
+// 11. map()
+        Optional<Integer> lengthOpt = opt.map(String::length);
+        System.out.println("Mapped lengthOpt: " + lengthOpt);
+
+// 12. flatMap()
+        Optional<String> flatMapped1 = opt.flatMap(value1 -> Optional.of(value1.toUpperCase()));
+        System.out.println("FlatMapped opt: " + flatMapped1);
+
+// 13. filter()
+        Optional<String> filtered = opt.filter(value2 -> value2.length() > 3);
+        System.out.println("Filtered opt: " + filtered);
+        Optional<String> emptyFiltered = opt.filter(value3 -> value3.length() > 10);
+        System.out.println("Empty Filtered opt: " + emptyFiltered);
+
+// 14. stream() (Java 9+)
+        opt.stream().forEach(System.out::println);
+
+// 15. or() (Java 9+)
+        Optional<String> anotherOpt = opt.or(() -> Optional.of("Another Value"));
+        System.out.println("Or opt: " + anotherOpt);
+        Optional<String> anotherEmptyOpt = emptyOpt.or(() -> Optional.of("Another Value"));
+        System.out.println("Or emptyOpt: " + anotherEmptyOpt);
+
+// 16. equals()
+        System.out.println("opt equals anotherOpt: " + opt.equals(anotherOpt));
+        System.out.println("emptyOpt equals anotherEmptyOpt: " + emptyOpt.equals(anotherEmptyOpt));
+
+// 17. hashCode()
+        System.out.println("opt hashCode: " + opt.hashCode());
+        System.out.println("emptyOpt hashCode: " + emptyOpt.hashCode());
+
+// 18. toString()
+        System.out.println("opt toString: " + opt.toString());
+        System.out.println("emptyOpt toString: " + emptyOpt.toString());
+
+
+        Collections.sort(list);
+    }
+}
